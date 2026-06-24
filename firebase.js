d9a58c217cdec411aa6c2e9983dc49e863aa3365
pugslyvrb-cmd <pugslyvrb@gmail.com>
2026-06-23 20:50:36 -0400

    Format firebase.js and ensure consistent imports

--- firebase.js
+++ firebase.js
@@ -1,15 +1,28 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getFirestore } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAfYgJpIfIDhvlXTYuXUO16UlMpWg1hQ0M",
-  authDomain: "yeeps-worlds.firebaseapp.com",
-  projectId: "yeeps-worlds",
-  storageBucket: "yeeps-worlds.firebasestorage.app",
-  messagingSenderId: "718914308574",
-  appId: "1:718914308574:web:63190b3429774164ab754f"
+
+apiKey: "AIzaSyAfYgJpIfIDhvlXTYuXUO16UlMpWg1hQ0M",
+
+authDomain: "yeeps-worlds.firebaseapp.com",
+
+projectId: "yeeps-worlds",
+
+storageBucket: "yeeps-worlds.firebasestorage.app",
+
+messagingSenderId: "718914308574",
+
+appId: "1:718914308574:web:63190b3429774164ab754f"
+
 };
 
+
 const app = initializeApp(firebaseConfig);
+
 
 export const db = getFirestore(app);
